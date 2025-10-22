0b787fc36de59419aa8b1d6458367fb53617ca67
juliacsramos <julia.ramos@iclass.com.br>
2025-10-22 14:50:30 -0300

    feat: adicionando estrutura básica

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import {MatDividerModule} from '@angular/material/divider';
-
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     MatListModule,
-    MatDividerModule
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule
   ],
   exports: [
     MatListModule,
-    MatDividerModule
-  ]
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    MatButtonModule
+    ],
+  providers: [provideNativeDateAdapter()],
 })
 export class AngularMaterialModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from "./angular-material/angular-material.module";
 
 @NgModule({
   declarations: [
@@ -13,8 +14,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ComponentsModule
-  ],
+    ComponentsModule,
+    AngularMaterialModule
+],
   providers: [
     provideAnimationsAsync()
   ],
